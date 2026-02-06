79aa872d358c9db415fb3773e0113efaab7ce996
vishalcodeer <vpande2009@gmail.com>
2026-02-06 19:53:26 +0530

    Create ElectricityBill.java

---
+++ ElectricityBill.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ElectricityBill {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter units consumed: ");
+        int units = sc.nextInt();
+
+        double bill;
+
+        if (units <= 100) {
+            bill = units * 2;
+        } else if (units <= 200) {
+            bill = (100 * 2) + (units - 100) * 3;
+        } else {
+            bill = (100 * 2) + (100 * 3) + (units - 200) * 5;
+        }
+
+        bill += 150; 
+        System.out.println("Total Electricity Bill: ₹" + bill);
+    }
+}
